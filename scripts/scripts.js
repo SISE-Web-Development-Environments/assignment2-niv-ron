6f13bd586d49e8e586a149a8e6c9a1e69fd8347f
waz11 <ronisr@post.bgu.ac.il>
2020-04-27 00:33:17 +0300

    ron

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -61,3 +61,11 @@ function showMenu() {
         show('welcome');
     }
 }
+
+// Disable arrow key scrolling in users browser
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
