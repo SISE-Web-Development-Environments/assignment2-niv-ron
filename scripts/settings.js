c6c3191e9834bfa6805c4674dc6e96c4203f5af6
waz11 <ronisr@post.bgu.ac.il>
2020-04-26 13:00:20 +0300

    building settings for game

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,17 +1,43 @@
-$("#in2").click(
-    $(document).keypress(function (event) {
-        var key = (event.keyCode ? event.keyCode : event.which);
-        var ch = String.fromCharCode(key)
-        $('#p1').attr('placeholder', ch);
-        $('#p1').html(ch);
-    })
-);
+$(document).ready(function () {
+    setKeys();
+});
 
-function setKeyCode(event,obj) {
-    let key = event.keyCode;
+var keys = {
+    key_up: 38, key_Down: 40, key_Left: 37, key_right: 39,
+    setDefualt: function () { this.key_up = 38, this.key_Down = 40, this.key_Left = 37, this.key_right = 39 },
+    changeKeys: function (up, down, left, right) { this.key_up = up, this.key_Down = down, this.key_Left = left, this.key_right = right }
+}
 
-    // alert("You choose for " + event.code);
-    $('#in'+obj).attr("placeholder", "key");
-    $('#p'+obj).html("" + event.code);
+
+function setKeys() {
+    document.getElementById("setkeyleft").addEventListener('keydown', function (event) {
+        if (!event.metaKey)
+            event.preventDefault();
+        this.style.backgroundImage = "url('../images/keys/" + event.key + ".png')";
+        // this.value = event.key;
+    });
+
+
+    document.getElementById("setkeyright").addEventListener('keydown', function (event) {
+        if (!event.metaKey)
+            event.preventDefault();
+        this.style.backgroundImage = "url('../images/keys/" + event.key + ".png')";
+        // this.value = event.key;
+    });
+
+    document.getElementById("setkeyup").addEventListener('keydown', function (event) {
+        if (!event.metaKey)
+            event.preventDefault();
+        this.style.backgroundImage = "url('../images/keys/" + event.key + ".png')";
+        // this.value = event.key;
+    });
+
+    document.getElementById("setkeydown").addEventListener('keydown', function (event) {
+        if (!event.metaKey)
+            event.preventDefault();
+        this.style.backgroundImage = "url('../images/keys/" + event.key + ".png')";
+        // this.value = event.key;
+    });
+
 
 }
